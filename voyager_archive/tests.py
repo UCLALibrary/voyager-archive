85b877a3ee1b2d368352c9fc9fa0ee552709d74b
Andy Kohler <akohler@library.ucla.edu>
2022-09-01 15:29:24 -0800

    Add basic tests for the form

--- voyager_archive/tests.py
+++ voyager_archive/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from voyager_archive.forms import VoyArchiveForm
 
-# Create your tests here.
+
+class FormTests(TestCase):
+    def test_valid_form_options(self):
+        form_data = {'search_type': 'BIB_ID', 'search_term': 12345}
+        form = VoyArchiveForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_search_type(self):
+        form_data = {'search_type': 'BAD_TYPE', 'search_term': 12345}
+        form = VoyArchiveForm(data=form_data)
+        self.assertFormError(
+            form=form,
+            field='search_type',
+            errors='Select a valid choice. BAD_TYPE is not one of the available choices.'
+        )
