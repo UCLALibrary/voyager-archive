2417c0e29fdae884e82e824ad6736cc8a5edf3d6
Kristian Allen <kallen2@library.ucla.edu>
2022-09-22 12:44:49 -0700

    Raw marc field dump test

--- voyager_archive/views_utils.py
+++ voyager_archive/views_utils.py
@@ -1,14 +1,36 @@
 from django.shortcuts import get_object_or_404
+from pymarc import Record
 from .models import AuthRecord, BibRecord, MfhdRecord
 
 
 def get_auth_record(auth_id: int) -> AuthRecord:
-    return get_object_or_404(AuthRecord, auth_id=auth_id)
+    rec = AuthRecord.objects.get(auth_id=auth_id)
+    marc_text = getattr(rec, "auth_record")
+    
+    byte_obj = marc_text.encode('utf-8')
+    marc_record = Record(data=byte_obj)
+    marc_fields = marc_record.get_fields()
+
+    return marc_fields
 
 
 def get_bib_record(bib_id: int) -> BibRecord:
-    return get_object_or_404(BibRecord, bib_id=bib_id)
+    rec = BibRecord.objects.get(bib_id=bib_id)
+    marc_text = getattr(rec, "bib_record")
+    
+    byte_obj = marc_text.encode('utf-8')
+    marc_record = Record(data=byte_obj)
+    marc_fields = marc_record.get_fields()
+
+    return marc_fields
 
 
 def get_mfhd_record(mfhd_id: int) -> MfhdRecord:
-    return get_object_or_404(MfhdRecord, mfhd_id=mfhd_id)
+    rec = MfhdRecord.objects.get(mfhd_id=mfhd_id)
+    marc_text = getattr(rec, "mfhd_record")
+    
+    byte_obj = marc_text.encode('utf-8')
+    marc_record = Record(data=byte_obj)
+    marc_fields = marc_record.get_fields()
+
+    return marc_fields
